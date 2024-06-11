656138cff8b386053e19fbd8c74267284ea8348a
Riyan Mustafa <riyanmustafa@Riyans-Laptop.local>
2024-06-10 20:40:19 -0400

    feat: added hangman

--- hang.py
+++ hang.py
@@ -4,14 +4,87 @@ import requests
 
 def get_random_word():
   try:
-      response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+        response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
         response.raise_for_status()
         word = response.json()[0].upper()
         return word
-    except requests.RequestException as e:
+  except requests.RequestException as e:
         print(f"Error fetching word: {e}")
         # Fallback to a predefined list in case of an error, just incase of error in the pull req
         fallback_words = [
             "PYTHON", "DEVELOPER", "HANGMAN", "COMPUTER", "PROGRAMMING"
         ]
         return random.choice(fallback_words).upper()
+  
+  def display_hangman(tries):
+       stages = [
+            """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          / \\  |
+               |
+        ---------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+          /    |
+               |
+        ---------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\\  |
+               |
+               |
+        ---------
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|   |
+               |
+               |
+        ---------
+        """,
+        """
+           -----
+           |   |
+           O   |
+           |   |
+               |
+               |
+        ---------
+        """,
+        """
+           -----
+           |   |
+           O   |
+               |
+               |
+               |
+        ---------
+        """,
+        """
+           -----
+           |   |
+               |
+               |
+               |
+               |
+        ---------
+        """
+    ]
+  return stages[tries]
+
+def play(word):
+     word_completion -"_" * len(word)
+     guessed = False
+     
